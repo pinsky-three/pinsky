67f980195c1412471f4bda5855be387f8d59c3fb
Bregy Malpartida <bregy@minsky.cc>
2024-01-17 20:41:30 -0500

    feat: update image and parameters in main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use wfc_rs::{Wfc, WfcImage};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
-    let image = WfcImage::from_file("images/pinsky_tile_32x32.png").unwrap();
+    let image = WfcImage::from_file("images/pinsky_tile_v2_32x32.png").unwrap();
 
-    let mut wfc = Wfc::overlapping(128, 128, image, 3, 3, false, false, false, false).unwrap();
+    let mut wfc = Wfc::overlapping(256, 256, image, 3, 3, true, false, false, false).unwrap();
 
     wfc.run(None, None)?;
 
-    wfc.export("output_1.png")?;
+    wfc.export("output_v2.png")?;
 
     Ok(())
 }
